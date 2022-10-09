fe7381c5724538e4663fc6a9d9475ef40e72bad0
Groundy <Szczygielski107@gmail.com>
2022-10-09 22:10:10 +0200

    Add Json lib

--- src/main/java/job/roomManager/RoomManagerApplication.java
+++ src/main/java/job/roomManager/RoomManagerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RoomManagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RoomManagerApplication.class, args);
 	}
-
 }
